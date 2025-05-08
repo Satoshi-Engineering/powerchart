ab8fd4d1222da9f4420899ad9a10c729eb6518d1
dr-erych <dave@satoshiengineering.com>
2025-05-08 16:20:47 +0200

    feat: add validateRuntimeConfig script
    Refs: projects#1762

---
+++ cli/validateRuntimeConfig.ts
@@ -0,0 +1,22 @@
+import kebabCase from 'lodash.kebabcase'
+
+import { runtimeConfig } from '~~/nuxt.runtimeConfig.js'
+
+const checkConfig = (rootKey: string, config: object) => {
+  Object.entries(config).forEach(([key, value]) => {
+    const currentKey = mapKey(rootKey, key)
+    if (typeof value === 'object') {
+      checkConfig(currentKey, value)
+      return
+    }
+    if (process.env[currentKey] == null) {
+      throw new Error(`Missing environment variable ${currentKey}`)
+    }
+  })
+}
+
+const mapKey = (rootKey: string, key: string) => {
+  return `${rootKey}_${kebabCase(key).toUpperCase().replace(/-/g, '_')}`
+}
+
+checkConfig('NUXT', runtimeConfig)

f4ff366ae32edc93c7136b1145a63d13303ff56c
Thomas Schagerl <tom@satoshiengineering.com>
2025-05-13 10:31:37 +0200

    test: add e2e test for switching back to standard time
    Refs: projects#1763

--- e2e/indexPage.test.ts
+++ e2e/indexPage.test.ts
@@ -14,15 +14,28 @@ test('price chart', async ({ page }) => {
   await expect(page.getByTestId('x-axis').locator(':scope > g').nth(14)).toHaveText('14:00')
 })
 
-test('leap day march', async ({ page }) => {
-  await page.clock.setFixedTime(new Date('2025-03-30T10:00:00'))
+test('switch to summer time', async ({ page }) => {
+  await page.clock.setFixedTime(new Date('2024-03-31T10:00:00'))
 
   await gotoAndWaitForNuxtHydration(page, '/')
 
   await expect(page.getByTestId('electricity-price-chart')).toBeVisible()
   await expect(page.getByTestId('bar-group-power').locator(':scope > *')).toHaveCount(23)
   await expect(page.getByTestId('bar-labels').locator(':scope > *')).toHaveCount(23)
-  await expect(page.getByTestId('bar-labels').locator(':scope > *').nth(14)).toHaveText('19.15')
+  await expect(page.getByTestId('bar-labels').locator(':scope > *').nth(14)).toHaveText('14.78')
   await expect(page.getByTestId('x-axis').locator(':scope > g').nth(0)).toHaveText('00:00 NZ')
   await expect(page.getByTestId('x-axis').locator(':scope > g').nth(14)).toHaveText('15:00 SZ')
+})
+
+test('return to standard time', async ({ page }) => {
+  await page.clock.setFixedTime(new Date('2024-10-27T10:00:00'))
+
+  await gotoAndWaitForNuxtHydration(page, '/')
+
+  await expect(page.getByTestId('electricity-price-chart')).toBeVisible()
+  await expect(page.getByTestId('bar-group-power').locator(':scope > *')).toHaveCount(25)
+  await expect(page.getByTestId('bar-labels').locator(':scope > *')).toHaveCount(25)
+  await expect(page.getByTestId('bar-labels').locator(':scope > *').nth(14)).toHaveText('21.08')
+  await expect(page.getByTestId('x-axis').locator(':scope > g').nth(0)).toHaveText('00:00 SZ')
+  await expect(page.getByTestId('x-axis').locator(':scope > g').nth(14)).toHaveText('13:00 NZ')
 })

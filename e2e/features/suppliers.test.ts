12a12e34a64133d872441da1e1c45d8a1e3a8322
Thomas Schagerl <tom@satoshiengineering.com>
2025-06-04 13:55:23 +0200

    fix: e2e tests for different suppliers do no longer exclude fees
    Refs: projects#1847

--- e2e/features/suppliers.test.ts
+++ e2e/features/suppliers.test.ts
@@ -18,20 +18,9 @@ test.beforeAll(async () => {
 test('use a different electricity supplier', async ({ page }) => {
   await page.clock.setFixedTime(currentDate)
 
-  await gotoAndWaitForNuxtHydration(page, '/awattar/')
-
-  await expect(page.getByTestId('electricity-price-chart')).toBeVisible()
-  await expect(page.getByTestId('bar_14:00').getByTestId('bar-total-price')).toHaveText('11.95')
-  await expect(page.getByTestId('x-axis').locator(':scope > g').nth(14)).toHaveText('14:00')
-})
-
-test('for a different electricity supplier the infrastructure fee is excluded', async ({ page }) => {
-  // the default setting is for johannes electric car filling station and the infrastructure fee is only relevant for that station
-  // with a different electricity supplier the cart is not used for the filling station, so the fee is not applicable
-  await page.clock.setFixedTime(currentDate)
-
   await gotoAndWaitForNuxtHydration(page, '/awattar')
 
-  await expect(page.getByTestId('bar_09:00').locator(':scope > rect')).toHaveCount(6)
-  await expect(page.getByTestId('bar-segment-infrastructureFee')).toHaveCount(0)
+  await expect(page.getByTestId('electricity-price-chart')).toBeVisible()
+  await expect(page.getByTestId('bar_14:00').getByTestId('bar-total-price')).toHaveText('16.75')
+  await expect(page.getByTestId('x-axis').locator(':scope > g').nth(14)).toHaveText('14:00')
 })

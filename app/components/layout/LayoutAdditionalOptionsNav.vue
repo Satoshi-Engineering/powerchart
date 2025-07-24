771136d6f821c2a9e1299fffe9efe45c525c2186
Thomas Schagerl <tom@satoshiengineering.com>
2025-07-24 13:59:22 +0200

    refactor: UCheckbox use ref i/o writableComputed for playwright check/uncheck functions
    Refs: projects#1871

--- app/components/layout/LayoutAdditionalOptionsNav.vue
+++ app/components/layout/LayoutAdditionalOptionsNav.vue
@@ -7,9 +7,10 @@
   </TypoHeadline>
 
   <UCheckbox
-    v-model="addVat"
+    v-model="addVatRef"
     :label="$t('sidebar.addVat')"
     data-testid="add-vat-checkbox"
+    @update:model-value="addVat = !!$event"
   />
 
   <UCheckbox
@@ -24,6 +25,11 @@
 
 <script setup lang="ts">
 const { addVat } = useAddVat()
+// https://gitlab.satoshiengineering.com/satoshiengineering/powerchart/-/jobs/46587
+// Workaround!
+// Assigning the addVat writableComputed directly to the UCheckbox will not change the value immediately on click
+// and then the playwright "check" and "uncheck" helpers won't work. So we use a local ref instead.
+const addVatRef = ref(addVat.value)
 
 const {
   surroundingLayoutDisabled,

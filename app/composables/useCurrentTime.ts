ff3d5795389c42cbd75f8a971cdecd03bd7f90b2
Thomas Schagerl <tom@satoshiengineering.com>
2025-10-21 08:40:18 +0200

    fix: only start interval on client
    Refs: projects#2092

--- app/composables/useCurrentTime.ts
+++ app/composables/useCurrentTime.ts
@@ -4,11 +4,16 @@ export default () => {
   const today = ref<string>(DateTime.now().toISODate())
   const currentHour = ref(DateTime.now().toFormat('H'))
 
-  const interval = setInterval(() => {
-    today.value = DateTime.now().toISODate()
-    currentHour.value = DateTime.now().toFormat('H')
-  }, 1_000)
-  onBeforeUnmount(() => clearInterval(interval))
+  let interval: ReturnType<typeof setInterval> | undefined
+  onBeforeMount(() => {
+    setInterval(() => {
+      today.value = DateTime.now().toISODate()
+      currentHour.value = DateTime.now().toFormat('H')
+    }, 1_000)
+  })
+  onBeforeUnmount(() => {
+    clearInterval(interval)
+  })
 
   return { today, currentHour }
 }
